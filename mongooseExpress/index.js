fa4d2e0f3815ef9bbc27ac2bc3c96073d441d932
anyadev@proton.me <anyadev@proton.me>
2024-10-07 19:22:30 -0400

    working on mongo crud app

--- mongooseExpress/index.js
+++ mongooseExpress/index.js
@@ -1,9 +1,36 @@
 const express = require('express')
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
 
-app.set('views', path.join(_dirname, 'views'));
+const Product = require('./models/product');
+
+mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
+    .then(() => {
+        console.log("mongo connection open")
+    })
+    .catch(err => {
+        console.log("oh no mongo error!")
+        console.log(err)
+    });
+
+
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
+
+app.get('/products', async (req, res) => {
+    const products = await Product.find({})
+    // console.log(products)
+    res.render('products/index', { products })
+})
+
+app.get('products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Products.findbyId(id)
+    console.log(product)
+    res.send('details page!')
+})
 
 app.listen(3000, () => {
     console.log('app is listening on port 3000')
