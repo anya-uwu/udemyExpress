09b707a0b19ad978befc6ccf2ac9bbf6cfa05ceb
anyadev@proton.me <anyadev@proton.me>
2024-10-08 11:51:34 -0400

    finished product detail page and creating new product

--- mongooseExpress/index.js
+++ mongooseExpress/index.js
@@ -3,6 +3,9 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
+
 
 const Product = require('./models/product');
 
@@ -18,6 +21,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/products', async (req, res) => {
     const products = await Product.find({})
@@ -25,11 +29,33 @@ app.get('/products', async (req, res) => {
     res.render('products/index', { products })
 })
 
-app.get('products/:id', async (req, res) => {
+app.get('/products/new', (req, res) => {
+    res.render('products/new')
+})
+
+app.post('/products', async (req, res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`)
+})
+
+app.get('/products/:id', async (req, res) => {
     const { id } = req.params;
-    const product = await Products.findbyId(id)
-    console.log(product)
-    res.send('details page!')
+    const product = await Product.findById(id)
+    res.render('products/show', { product })
+})
+
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id)
+    res.render('products/edit', { product })
+})
+
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = Product.findByIdAndUpdate(id, req.body, {runValidators: true})
+    console.log(req.body)
+    res.send('PUT!!!')
 })
 
 app.listen(3000, () => {
